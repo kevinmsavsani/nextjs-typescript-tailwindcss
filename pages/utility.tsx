ec92c1ccd3e180bf13a667221e4a4f33036e7dea
kevinmsavsani <kevinmsavsani@gmail.com>
2022-01-26 12:24:39 +0530

    width tailwind tested

--- pages/utility.tsx
+++ pages/utility.tsx
@@ -1,8 +1,6 @@
-
+import Utility from '../components/Utility'
 const UtilityPage = () => (
-    <div className="pt-">
-        <img src="https://raw.githubusercontent.com/tailwindlabs/tailwindcss-from-zero-to-production/98b52c61259c732226c2ec0207ba2246e67a34e6/02-the-utility-first-workflow/img/logo.svg" />
-    </div>
+    <Utility></Utility>
 )
 
 export default UtilityPage
